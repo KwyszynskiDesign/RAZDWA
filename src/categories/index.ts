eb7fc581a4ef6669f28496b0d564a76639eaf78a
Wyszko <wyszynski.k@onet.pl>
2026-03-12 19:22:09 +0100

    Align category registry with current UI routes

--- src/categories/index.ts
+++ src/categories/index.ts
@@ -1,42 +1,39 @@
-import { sampleCategory } from "./sample";
-import { plakatyCategory } from "./plakaty";
-import { voucheryCategory } from "./vouchery";
-import { dyplomyCategory } from "./dyplomy";
-import { wizytowkiCategory } from "./wizytowki";
-import { ZaproszeniaKredaView as zaproszeniaKredaCategory } from "../ui/views/zaproszenia-kreda";
-import { UlotkiDwustronneView as ulotkiDwustronneCategory } from "../ui/views/ulotki-cyfrowe-dwustronne";
-import { UlotkiJednostronneView as ulotkiJednostronneCategory } from "../ui/views/ulotki-cyfrowe-jednostronne";
-import { BannerView as banneryCategory } from "../ui/views/banner";
-import { WlepkiView as wlepkiCategory } from "../ui/views/wlepki-naklejki";
-import { RollUpView as rollupCategory } from "../ui/views/roll-up";
-import { createHTMLCategory } from "./loader";
-import { drukCADCategory } from "./druk-cad";
-import { drukA4A3Category } from "./druk-a4-a3-skan";
+import { BannerView } from "../ui/views/banner";
+import { CadUploadView } from "../ui/views/cad-upload";
+import { CanvasView } from "../ui/views/canvas";
+import { DrukCADView } from "../ui/views/druk-cad";
+import { DrukA4A3SkanView } from "../ui/views/DrukA4A3SkanView";
+import { DyplomyView } from "../ui/views/dyplomy";
+import { FoliaSzronionaView } from "../ui/views/folia-szroniona";
+import { LaminowanieView } from "../ui/views/laminowanie";
+import { PlakatyView } from "../ui/views/plakaty";
+import { RollUpView } from "../ui/views/roll-up";
+import { UlotkiCyfroweView } from "../ui/views/ulotki-cyfrowe";
+import { VoucheryView } from "../ui/views/vouchery";
+import { WizytowkiView } from "../ui/views/wizytowki-druk-cyfrowy";
+import { WlepkiView } from "../ui/views/wlepki-naklejki";
+import { WycinanieFoliiView } from "../ui/views/wycinanie-folii";
+import { ZaproszeniaKredaView } from "../ui/views/zaproszenia-kreda";
 import { artykulyBiuroweCategory } from "./artykuly-biurowe";
 import { uslugiCategory } from "./uslugi";
 
 export const categories = [
-  // Istniejące kategorie JS
-  sampleCategory,
-  plakatyCategory,
-  voucheryCategory,
-  dyplomyCategory,
-  wizytowkiCategory,
-  zaproszeniaKredaCategory,
-  ulotkiDwustronneCategory,
-  ulotkiJednostronneCategory,
-  banneryCategory,
-  wlepkiCategory,
-  rollupCategory,
-  
-  // Nowe kategorie z pełną logiką
-  drukCADCategory,
-  drukA4A3Category,
+  DrukA4A3SkanView,
+  DrukCADView,
+  CadUploadView,
+  LaminowanieView,
+  BannerView,
+  WizytowkiView,
+  UlotkiCyfroweView,
+  WlepkiView,
+  RollUpView,
+  DyplomyView,
+  ZaproszeniaKredaView,
+  FoliaSzronionaView,
+  WycinanieFoliiView,
+  CanvasView,
+  VoucheryView,
+  PlakatyView,
   artykulyBiuroweCategory,
   uslugiCategory,
-
-  // Pozostałe kategorie HTML
-  createHTMLCategory('folia-szroniona', '✨ Folia Szroniona', 'folia-szroniona.html'),
-  createHTMLCategory('laminowanie', '🔒 Laminowanie', 'laminowanie.html'),
-  createHTMLCategory('upload-kalkulator', '🖨️ Kalkulator CAD Upload', 'upload-kalkulator.html'),
 ];
