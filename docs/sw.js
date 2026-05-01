d2e730029a8edfc19e25a2d7f2a3b8b6255f229a
Wyszko <wyszynski.k@onet.pl>
2026-05-01 07:31:31 +0200

    Harmonize CAD warning colors with design system variables

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_VERSION = 'razdwa-v202605010727';
+﻿const CACHE_VERSION = 'razdwa-v202605010731';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // Force new SW to activate immediately
