38a1b2c22b38e8453f32b1ab279b88544d6280a2
Wyszko <wyszynski.k@onet.pl>
2026-05-02 09:44:57 +0200

    chore: Update cache version to v202605011445

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_VERSION = 'razdwa-v202605020936';
+﻿const CACHE_VERSION = 'razdwa-v202605020945';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting(); // Force new SW to activate immediately
