99a98922c19c3bf5fdb40bec7c5ca4401e43298f
Wyszko <wyszynski.k@onet.pl>
2026-05-09 20:30:09 +0200

    fix: restore missing artykuly biurowe products by fixing corrupted keys in prices.json

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605092014';
+var CACHE_VERSION = 'razdwa-v202605092030';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
