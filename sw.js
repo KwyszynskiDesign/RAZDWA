f8da7600e024c730ca873d6b8922f7534d2605cb
Wyszko <wyszynski.k@onet.pl>
2026-05-10 11:26:52 +0200

    fix: tighten settings table widths

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605101123';
+var CACHE_VERSION = 'razdwa-v202605101126';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
