485e91d244485cbb6513023277abc495195c2fba
Wyszko <wyszynski.k@onet.pl>
2026-05-12 20:20:27 +0200

    chore: bump SW cache v202605121500

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605091443';
+var CACHE_VERSION = 'razdwa-v202605121500';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605122019';
+var CACHE_VERSION = 'razdwa-v202605122020';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
