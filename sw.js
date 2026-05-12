67ea535548a1b416b9479a43eaeec74ac13537ed
Wyszko <wyszynski.k@onet.pl>
2026-05-12 19:26:43 +0200

    fix: usu legacy skrypt A4A3 i statyczn legend folii

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605121918';
+var CACHE_VERSION = 'razdwa-v202605121926';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
