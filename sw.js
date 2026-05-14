6218796e66f6a7250caaf45ae45974bcbe37a0a6
Wyszko <wyszynski.k@onet.pl>
2026-05-14 16:59:26 +0200

    fix: remove card styling from wycinanie-folii breakdown hint

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605141655';
+var CACHE_VERSION = 'razdwa-v202605141659';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
