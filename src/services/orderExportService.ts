0bda7fd2adaae9a609a5dadc19e995b85d6b6f79
Wyszko <wyszynski.k@onet.pl>
2026-05-14 16:23:51 +0200

    feat: split Realizacja/Kto dodal 50/50, update Apps Script URL

--- src/services/orderExportService.ts
+++ src/services/orderExportService.ts
@@ -11,8 +11,9 @@ export interface OrderExportConfig {
 const LEGACY_APPS_SCRIPT_URLS = [
   "https://script.google.com/macros/s/AKfycbwxTnDfsnV6QFwnN1DOX61In3Py_S3kedDOQbZ7F1XYcIlTVdYCzZ71ay1TPjV6l4rW/exec",
   "https://script.google.com/macros/s/AKfycbwFSyBg_ZtPgJYQKymNRDWNdX0XQit3G3jvxrQ2VOX-pE-R4rZuPwf6QqnkSe-xrbNy/exec",
+  "https://script.google.com/macros/s/AKfycbwTpUgmnb3rU37002mEH6hsNVNIIW0eRNO1pG_0WQWBz5CN0BwSpTmaOABIJSAFJDwp/exec",
 ] as const;
-const CURRENT_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwTpUgmnb3rU37002mEH6hsNVNIIW0eRNO1pG_0WQWBz5CN0BwSpTmaOABIJSAFJDwp/exec";
+const CURRENT_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1_WAWeJxAXSkvoxOAqEI-kSPRMZjf9vtMtXNw1ykbMCHPVa3A0HH0c5PfhHoO_4_c/exec";
 
 export interface OrderExportPayload {
   source: "razdwa-web";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605141616';
+var CACHE_VERSION = 'razdwa-v202605141623';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
