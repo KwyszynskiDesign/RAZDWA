d413adba25e077686578cdceb6e64aca02215615
Wyszko <wyszynski.k@onet.pl>
2026-05-15 06:39:35 +0200

    chore: rebuild bundle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 'razdwa-v202605150638';
+var CACHE_VERSION = 'razdwa-v202605150639';
 
 self.addEventListener('install', function (event) {
   event.waitUntil(self.skipWaiting());
