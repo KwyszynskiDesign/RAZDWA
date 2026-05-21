c5f4a479794a8e99d516285f6ad91df5a4bada38
Wyszko <wyszynski.k@onet.pl>
2026-05-21 06:41:48 +0200

    fix(style): prevent clipping inside breakdown cards (overflow visible)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Fetch: NetworkFirst (HTML), CacheFirst (static)
  */
 
-var CACHE_VERSION = 'razdwa-v202605210638'; // Injected by prebuild script
+var CACHE_VERSION = 'razdwa-v202605210641'; // Injected by prebuild script
 
 /**
  * Install Event: Skip precaching - use on-demand caching instead
