0d6f307c34bc247018e4891ecea49aad8af60db5
Wyszko <wyszynski.k@onet.pl>
2026-05-21 20:46:50 +0200

    build: include cad qty-init fix in bundle; bump cache version

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Fetch: NetworkFirst (HTML), CacheFirst (static)
  */
 
-var CACHE_VERSION = 'razdwa-v202605212044'; // Injected by prebuild script
+var CACHE_VERSION = 'razdwa-v202605212047'; // Injected by prebuild script
 
 /**
  * Install Event: Skip precaching - use on-demand caching instead
