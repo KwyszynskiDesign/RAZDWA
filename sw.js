77880b4ad6adc78a46fc3b6677a0214e8236f37b
Wyszko <wyszynski.k@onet.pl>
2026-05-21 20:01:07 +0200

    fix: add missing CAD upload normalized data for docs build

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Fetch: NetworkFirst (HTML), CacheFirst (static)
  */
 
-var CACHE_VERSION = 'razdwa-v202605211954'; // Injected by prebuild script
+var CACHE_VERSION = 'razdwa-v202605212001'; // Injected by prebuild script
 
 /**
  * Install Event: Skip precaching - use on-demand caching instead
