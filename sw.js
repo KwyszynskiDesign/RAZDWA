7860d8a3c7e25f5cf2cda6a2764ab7c6dd68b38a
Wyszko <wyszynski.k@onet.pl>
2026-05-26 18:20:22 +0200

    chore: ujednolicenie settings.json, szerokie uprawnienia Bash/PowerShell

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Fetch: NetworkFirst (HTML), CacheFirst (static)
  */
 
-var CACHE_VERSION = 'razdwa-v202605261819'; // Injected by prebuild script
+var CACHE_VERSION = 'razdwa-v202605261820'; // Injected by prebuild script
 
 /**
  * Install Event: Skip precaching - use on-demand caching instead
